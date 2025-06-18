cdab294e61c9b77a47bb684d07e00032d2e0771a
Sofya Abdulkina <sabdulki@lab1r5s2.42abudhabi.ae>
2025-06-18 22:39:55 +0400

    nginx serves all routes correctly. added cors setup to nginx

--- nginx/config/entrypoint.sh
+++ nginx/config/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Подставляем переменные окружения в шаблон
-envsubst '$SERVER_NAME $DOMAIN $FR_PORT $GS_PORT $MMRS_PORT $UMS_PORT' \
+envsubst '$DOMAIN $FR_PORT $GS_PORT $MMRS_PORT $UMS_PORT' \
   < /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
