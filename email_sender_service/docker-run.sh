3a2834700d0f5390ed0c99da7ce7eccb4de6a5f5
tmazitov <timurmazitov000@gmail.com>
2025-06-19 14:12:31 +0400

    add init property to each container in dev docker-compose

--- email_sender_service/docker-run.sh
+++ email_sender_service/docker-run.sh
@@ -1,2 +1,2 @@
 export MODE="production"
-node ./dist/app.js "0.0.0.0" $ESS_PORT ./templates.json $ESS_EMAIL $ESS_PASS
+exec node ./dist/app.js "0.0.0.0" $ESS_PORT ./templates.json $ESS_EMAIL $ESS_PASS
